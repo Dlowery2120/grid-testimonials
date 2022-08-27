511c38ac68296ff565073f4d5ff35f24816bf27a
Devante Lowery <dlowery2120@gmail.com>
2022-08-27 13:13:20 -0500

    finished

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import "./style.scss";
+
+function App() {
+
+  return (
+    <>
+      <div className="grid">
+        <div id="one" className="card | bg-violet">
+          <header className="card-header">
+            <img src="src/assets/images/image-daniel.jpg" alt="" className="card-img"></img>
+            <div>
+              <h3>Daniel Clifford</h3>
+              <p>Verified Graduate</p>
+            </div>
+          </header>
+          <p className="card-lead">
+            I received a job offer mid-course, and the subjects I learned were
+            current, if not more so, in the company I joined. I honestly feel I
+            got every penny’s worth.
+          </p>
+
+          <p className="card-quote">
+            “ I was an EMT for many years before I joined the bootcamp. I’ve
+            been looking to make a transition and have heard some people who had
+            an amazing experience here. I signed up for the free intro course
+            and found it incredibly fun! I enrolled shortly thereafter. The next
+            12 weeks was the best - and most grueling - time of my life. Since
+            completing the course, I’ve successfully switched careers, working
+            as a Software Engineer at a VR startup. ”
+          </p>
+        </div>
+        <div id="two" className="card | bg-grayish-blue">
+          <header className="card-header">
+            <img src="src/assets/images/image-jonathan.jpg" alt="" className="card-img"></img>
+            <div>
+              <h3>Jonathan Walters</h3>
+              <p>Verified Graduate</p>
+            </div>
+          </header>
+          <p className="card-lead">
+            The team was very supportive and kept me motivated.
+          </p>
+
+          <p className="card-quote">
+          “ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself. “
+          </p>
+        </div>
+        <div id="three" className="card | bg-white">
+          <header className="card-header">
+            <img src="src/assets/images/image-jeanette.jpg" alt="" className="card-img"></img>
+            <div>
+              <h3>Jeanette Harmon</h3>
+              <p>Verified Graduate</p>
+            </div>
+          </header>
+          <p className="card-lead">
+          An overall wonderful and rewarding experience.
+          </p>
+
+          <p className="card-quote">
+          “ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”
+          </p>
+        </div>
+        <div id="four" className="card | bg-blackish-blue">
+          <header className="card-header">
+            <img src="src/assets/images/image-patrick.jpg" alt="" className="card-img"></img>
+            <div>
+              <h3>Patrick Abrams</h3>
+              <p>Verified Graduate</p>
+            </div>
+          </header>
+          <p className="card-lead">
+          Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.
+          </p>
+
+          <p className="card-quote">
+          “ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”
+          </p>
+        </div>
+        <div id="five" className="card | bg-white">
+          <header className="card-header">
+            <img src="src/assets/images/image-kira.jpg" alt="" className="card-img"></img>
+            <div>
+              <h3>Kira Whittle</h3>
+              <p>Verified Graduate</p>
+            </div>
+          </header>
+          <p className="card-lead">
+            Such a life-changing experience. Highly recommended!
+          </p>
+
+          <p className="card-quote">
+          “ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I’ve often referred to it during interviews as an example of my developent experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend! ”
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
